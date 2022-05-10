7f24004c45eb6c705d5bc85cae9f751a0dfa4f27
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-05-10 11:12:17 +0200

    .

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 bicep build azuredeploy.bicep
 git commit -am "."
 git push
+
+cmd.exe /C "start $( echo "https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fchgeuer%2Faci1%2Fmaster%2Fazuredeploy.json" )"
