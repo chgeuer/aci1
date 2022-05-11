677a700e6b47e7041f9612c92c47122bc8c34c50
Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-05-11 11:14:33 +0200

    .

--- script.sh
+++ script.sh
@@ -2,6 +2,10 @@
 
 echo "-----"
 
+curl --include --get \
+  --url "http://169.254.169.254/metadata/instance" \
+  --data-urlencode "api-version=2021-02-01" \
+  --header "Metadata:true"
 
 access_token="$( curl --silent --get \
     --url "http://169.254.169.254/metadata/identity/oauth2/token" \
@@ -10,13 +14,6 @@ access_token="$( curl --silent --get \
     --data-urlencode "resource=https://management.azure.com" \
     --data-urlencode "bypass_cache=true" \
     | jq -r ".access_token" )"
-echo "${access_token}"
-
-
-curl --include --get \
-  --url "https://management.azure.com/subscriptions/${subscriptionId}/resourceGroups/${resourceGroupName}" \
-  --data-urlencode "api-version=2019-07-01" \
-  --header "Authorization: Bearer ${access_token}"
 
 
 rgjson="$( curl --silent --get \
